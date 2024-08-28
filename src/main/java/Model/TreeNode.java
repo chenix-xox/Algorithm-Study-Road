7731c777168f2e5f3b75809d49016b4e0eddb6b8
chenix <chuge1215@163.com>
2024-08-29 01:15:01 +0800

    fix::二叉树中序遍历（递归）

---
+++ src/main/java/Model/TreeNode.java
@@ -0,0 +1,28 @@
+package Model;
+
+import lombok.Data;
+
+/**
+ * @author chenix
+ * @date 2024.08.29 00:16
+ * @description
+ */
+@Data
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
